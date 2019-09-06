97c0ae6eccbfc08e5322e866677c2caf7aad0921
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:46 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,85 @@
+/* eslint-disable no-magic-numbers */
+import { of } from "most";
+import {empty} from "most";
+import streamSatisfies from "@unction/streamsatisfies";
+
+import fresh from "./index.ts";
+
+test("Array (filled)", () => {
+  expect(fresh(["a"])).toEqual([]);
+});
+
+test("Object (filled)", () => {
+  expect(fresh({aaa: "aaa"})).toEqual({});
+});
+
+test("Set (filled)", () => {
+  expect(fresh(new Set([1, 2, 3]))).toEqual(new Set());
+});
+
+test("Map (filled)", () => {
+  expect(fresh(new Map([["a", "b"]]))).toEqual(new Map());
+});
+
+test("String (filled)", () => {
+  expect(fresh("a")).toBe("");
+});
+
+test("Array (empty)", () => {
+  expect(fresh([])).toEqual([]);
+});
+
+test("Object (empty)", () => {
+  expect(fresh({})).toEqual({});
+});
+
+test("Set (empty)", () => {
+  expect(fresh(new Set())).toEqual(new Set());
+});
+
+test("Map (empty)", () => {
+  expect(fresh(new Map())).toEqual(new Map());
+});
+
+test("String (empty)", () => {
+  expect(fresh("")).toBe("");
+});
+
+test("Stream (filled)", done => {
+  streamSatisfies(
+    []
+  )(
+    (given) => (expected) => expect(given).toBe(expected)
+  )(
+    doesNotThrow
+  )(
+    ({length}) => (size) => {
+      expect(length).toBe(size);
+      done();
+    }
+  )(
+    fresh(of("a"))
+  );
+});
+
+test("Stream (empty)", done => {
+  streamSatisfies(
+    []
+  )(
+    (given) => (expected) => expect(given).toBe(expected)
+  )(
+    doesNotThrow
+  )(
+    ({length}) => (size) => {
+      expect(length).toBe(size);
+      done();
+    }
+  )(
+    fresh(empty())
+  );
+});
+
+
+test(() => {
+  expect(() => fresh(0)).toThrowError(new Error("fresh doesn't know how to handle Number"));
+});
